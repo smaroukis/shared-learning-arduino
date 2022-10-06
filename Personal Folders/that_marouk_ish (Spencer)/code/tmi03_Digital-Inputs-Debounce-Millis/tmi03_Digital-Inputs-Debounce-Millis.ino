41d735a3f6b9cc3ce477a49f7f0ee401cb99c8bc
Spencer <smaroukis@gmail.com>
2022-10-06 23:22:47 +0900

    2022-10-06 22:59:14: ...
    Affected files:
    Personal Folders/that_marouk_ish (Spencer)/code/tmi03_Digital-Inputs-Debounce-Millis/tmi03_Digital-Inputs-Debounce-Millis.ino

--- Personal Folders/that_marouk_ish (Spencer)/code/tmi03_Digital-Inputs-Debounce-Millis/tmi03_Digital-Inputs-Debounce-Millis.ino
+++ Personal Folders/that_marouk_ish (Spencer)/code/tmi03_Digital-Inputs-Debounce-Millis/tmi03_Digital-Inputs-Debounce-Millis.ino
@@ -2,15 +2,19 @@
 // Pressing pushbutton switches light on and off
 // added debounce and timing to the tmi02 case
 // using Blink Without Delay techniques for debouncing
+// see tmi05_05_pushbutton_led_fade for another debouncing example
 
 // Inputs
-int LED = 9;
-int BUTTON = 7;
+const int LED = 9;
+const int BUTTON = 7;
 
 // Others
-byte state;
-byte old_state;
-byte led_state = 0;
+byte state = HIGH; // due to the pullup
+byte old_state = HIGH;
+byte led_state = LOW;
+unsigned long debounce_start = 0;
+unsigned long current_time; 
+bool debouncing = false;
 
 void setup(){
   pinMode(LED, OUTPUT);
@@ -18,23 +22,20 @@ void setup(){
 }
 
 void loop(){
-  old_state = state; // save the previous state
-  state = digitalRead(BUTTON);
+  current_time = millis();
 
-  if ((state != old_state)){ // if the button is being pressed
-    // debounce 
-    delay(20); 
-
-    if (state != old_state){ // if still being pressed
-      if (led_state == 0){
-          digitalWrite(LED, HIGH);
-          led_state = 1;
-      } else {
-        digitalWrite(LED, LOW);
-        led_state = 0;
-      }
-        state = 1 - state; // switch the pushbutton state
-    }
-    
+  state = digitalRead(BUTTON); // PULLUP so normal is HIGH
+  if (state != old_state)
+  { // either button is pressed or it is noise
+    debounce_start = current_time;     // reset debouncing timer
+    old_state = state;
+    debouncing = true; // we are debouncing
+  }
+  else if (debouncing && state == LOW && (current_time - debounce_start > 50))
+  { // else if is key here
+    // take the current value as the actual state
+      led_state = (led_state == LOW ? HIGH : LOW); // ternary operator to switch led state
+      digitalWrite(LED, led_state);
+      debouncing = false;
   }
 }
