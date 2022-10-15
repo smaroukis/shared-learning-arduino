ce5d17480f26439057de1870942899fd3783ef4a
Spencer <smaroukis@gmail.com>
2022-10-15 18:15:23 +0900

    tmi13 - part 2 - servo now can be stopped and started with a pushbutton
    Affected files:
    Personal Folders/that_marouk_ish/code/tmi14_Multitasking-1/src/main.cpp

--- Personal Folders/that_marouk_ish/code/tmi14_Multitasking-1/src/main.cpp
+++ Personal Folders/that_marouk_ish/code/tmi14_Multitasking-1/src/main.cpp
@@ -4,37 +4,44 @@
 // waiting for a pushbutton press to stop motor movement
 // LED should stay at the current color
 
-
 #include <Arduino.h>
 #include <Servo.h>
 
 // Pins
 const byte PIN_SERVO1 = 9; // 9 <- SIGNAL (byte [0, 255])
-
-// Constants
+const int BUTTON = 11; // 11 <- Button (will be normal HIGH)
 
 // Variables
 unsigned long tNow; // for each time through the loop
 
 // Objects and Their Variables (before OOP)
 Servo thisServo; 
-boolean thisServoStarted = 0; // state machine variable
+boolean thisServoStarted = false; // state machine variable
 int pos = 0; // store position
 int delta_pos = 1; // {1, -1}
 unsigned long tPreviousStep = 0;
 const int tDelay = 10; // used to control the "speed" of the motor
 
-
+unsigned long tDebounceStart; // to store time for debouncing algo
+const int tDebounceDelay = 50;
+boolean debouncing = false;
+int buttonState = 0; 
+int buttonPreviousState = 1; // button is INPUT_PULLUP so normal is HIGH, active LOW
 
 void setup() {
-// Setup the Servos
-thisServo.attach(PIN_SERVO1);
+  // Setup the Servos
+  thisServo.attach(PIN_SERVO1);
+  pinMode(BUTTON, INPUT_PULLUP);
+  digitalWrite(BUTTON, HIGH); // to avoid ambiguity
+  Serial.begin(9600);
 }
 
 // The beginning of the "state machine"
 void servoStart() {
   // Set state
   thisServoStarted = true;
+  Serial.println("Started Servo");
+//  tPreviousStep = tNow; // #q do we need this? -> I think we do, otherwise it starts instantaneously and jitters
 }
 
 // Start the Servo Rotating - reverse directions when we get to the end;
@@ -42,9 +49,10 @@ void moveServoOneStep() {
 // first check if we should actually move one step
 // two conditions: 1) we are started and 2) the time condition has been met
   if (thisServoStarted && (tNow - tPreviousStep >= tDelay)) {
-    // ok now we can move one step only
     pos += delta_pos; // increment the position to write new position
     thisServo.write(pos); 
+    Serial.print("Moved Servo to position = ");
+    Serial.println(pos);
 
     if (pos == 180 || pos == 0) delta_pos = -delta_pos; 
     // update timer
@@ -54,13 +62,46 @@ void moveServoOneStep() {
 
 void servoStop() {
   thisServoStarted = false;
-  tPreviousStep = tNow; 
+//  tPreviousStep = tNow; 
+  Serial.println("Stopped Servo");
+}
+
+void servoChangeState() {
+  Serial.print("Changing Servo state from (");
+  Serial.print(thisServoStarted);
+  Serial.println(")");
+  switch (thisServoStarted) {
+    case false:
+      servoStart();
+      break;
+    case true:
+      servoStop();
+      break;
+  }
+}
+
+void checkButton() {
+  buttonState = digitalRead(BUTTON); // INPUT_PULLUP
+  if (buttonState != buttonPreviousState) {
+    tDebounceStart = tNow; // #q again, should this be tNow or millis()
+    buttonPreviousState = buttonState; 
+    debouncing = true;
+    Serial.println("Started Debouncing...");
+  }
+  else if (debouncing && buttonState == 0 && (tNow - tDebounceStart >= tDebounceDelay)) {
+    Serial.print("Debouncing finished, taking current state = ");
+    Serial.println(buttonState);
+    // stop or start the motor
+    servoChangeState();
+    // update the variables
+    debouncing = false;
+  }
 }
 
 // Loop
 void loop() {
   tNow = millis(); 
 
-  servoStart();
+  checkButton();
   moveServoOneStep();
 }
